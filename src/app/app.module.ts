942ab50a4a0ec3795c045bcf0e0a9433d95386d8
Myriam JAYLE <myriamjayle@gmail.com>
2023-11-14 08:49:23 +0100

    Header ainsi que la première section de la première page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { Section1Component } from './section1/section1.component';
+import { Section2Component } from './section2/section2.component';
+import { Section3Component } from './section3/section3.component';
+import { Section4Component } from './section4/section4.component';
+import { NavbarComponent } from './header/navbar/navbar.component';
+import { Navbar2Component } from './section2/navbar2/navbar2.component';
+
+// const routes: Routes = [
+//   {
+//     path: "accueil", component: AccueilComponent
+//   }
+// ]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    Section1Component,
+    Section2Component,
+    Section3Component,
+    Section4Component,
+    NavbarComponent,
+    Navbar2Component
   ],
   imports: [
     BrowserModule
