b2657fbf830b8aa64b85e0b7402bd458842aeb07
Myriam JAYLE <myriamjayle@gmail.com>
2023-11-15 08:44:58 +0100

    page d'accueil jusqu'à step c'est quoi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,59 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { Section1Component } from './section1/section1.component';
-import { Section2Component } from './section2/section2.component';
-import { Section3Component } from './section3/section3.component';
+import { CarousselComponent } from './caroussel/caroussel.component';
+import { Card1Component } from './card1/card1.component';
+import { CquoiComponent } from './cquoi/cquoi.component';
 import { Section4Component } from './section4/section4.component';
 import { NavbarComponent } from './header/navbar/navbar.component';
-import { Navbar2Component } from './section2/navbar2/navbar2.component';
+import { Navbar2Component } from './navbar2/navbar2.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { MissionsComponent } from './missions/missions.component';
+import { MetiersComponent } from './metiers/metiers.component';
+import { ContactComponent } from './contact/contact.component';
+import { Card2Component } from './card2/card2.component';
+import { SolutionComponent } from './solution/solution.component';
 
-// const routes: Routes = [
-//   {
-//     path: "accueil", component: AccueilComponent
-//   }
-// ]
+const routes: Routes = [
+  {
+    path: "", pathMatch: "full", component: AccueilComponent
+  },
+  {
+    path: "missions", component: MissionsComponent
+  },
+  {
+    path: "metiers", component: MetiersComponent
+  },
+  {
+    path: "contact", component: ContactComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    Section1Component,
-    Section2Component,
-    Section3Component,
+    CarousselComponent,
+    Card1Component,
+    CquoiComponent,
     Section4Component,
     NavbarComponent,
-    Navbar2Component
+    Navbar2Component,
+    AccueilComponent,
+    MissionsComponent,
+    MetiersComponent,
+    ContactComponent,
+    Card2Component,
+    SolutionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/cquoi/cquoi.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cquoi',
+  templateUrl: './cquoi.component.html',
+  styleUrls: ['./cquoi.component.css']
+})
+
+export class CquoiComponent {
+  cquoi=[
+    {
+      "titre": "Prestation de qualité",
+      "texte": "Pirus estiles mergotim relativae a utilienensis et la cartae maxima mergo et volio renecta dolienonsis a la praesum et Salta."
+    },
+    {
+      "titre": "Entrerpise inclusive",
+      "texte": "Pirus estiles mergotim relativae a utilienensis et la cartae maxima mergo et volio renecta dolienonsis a la praesum et Salta."
+    },
+    {
+      "titre": "Economie Sociale & Solidaire",
+      "texte": "Pirus estiles mergotim relativae a utilienensis et la cartae maxima mergo et volio renecta dolienonsis a la praesum et Salta."
+    }
+  ];
+
+}
