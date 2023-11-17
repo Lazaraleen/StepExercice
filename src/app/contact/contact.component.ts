4b4e412fadee4cf6d6745733a9101c6bf9cd0fbe
Myriam JAYLE <myriamjayle@gmail.com>
2023-11-17 16:29:09 +0100

    début configuration contact

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
-
+  public sendEmail(e: Event) {
+    e.preventDefault();
+    emailjs.sendForm('service_mr786bc', 'template_v1dgasc', e.target as HTMLFormElement, 'YOUR_PUBLIC_KEY')
+      .then((result: EmailJSResponseStatus) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+  }
 }
+
+// emailjs.send("service_mr786bc","template_v1dgasc", {
+//   name: "",
+//   name2: "",
+//   phone: "",
+//   email: "",
+//   message: "",
+//   enterprise: "",
+// });
